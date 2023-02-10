74573a06c044714c91f6397797550b01e1de91b8
Sean Nelson <hyperkineticnerd@gmail.com>
2023-02-10 08:10:35 -0700

    initial plugin version

--- b64tofile_credential_plugin/__init__.py
+++ b64tofile_credential_plugin/__init__.py
@@ -1,73 +1,51 @@
-import collections
+import os
+import tempfile
+from base64 import b64decode
+from collections import namedtuple
 
-CredentialPlugin = collections.namedtuple('CredentialPlugin', ['name', 'inputs', 'backend'])
+CredentialPlugin = namedtuple('CredentialPlugin', ['name', 'inputs', 'backend'])
 
-def some_lookup_function(**kwargs):
-    #
-    # IMPORTANT:
-    # replace this section of code with Python code that *actually*
-    # interfaces with some third party credential system
-    # (*this* code is just provided for the sake of example)
-    #
-    url = kwargs.get('url')
-    token = kwargs.get('secret')
+class CredGlobFiles:
+    def __init__(self, glob):
+        self.glob = glob
+    
+    def __enter__(self):
+        if not self.glob:
+            return None
+        self.globfile = tempfile.NamedTemporaryFile('wb', delete=False)
+        self.globfile.write(self.glob.encode())
+        self.globfile.flush()
+        return str(self.globfile.name)
 
-    if token != 'VALID':
-        raise ValueError('Invalid token!')
+    def __exit__(self):
+        if self.globfile and os.path.exists(self.globfile.name):
+            os.remove(self.globfile.name)
 
-    value = {
-        'username': 'mary',
-        'email': 'mary@example.org',
-        'password': 'super-secret'
-    }
+_inputs = {
+    'fields': [{
+        'id': 'secret',
+        'label': 'Secret Base64 Variable',
+        'type': 'string',
+        'secret': True,
+    }],
+    'required': ['secret'],
+}
 
-    if identifier in value:
-        return value[identifier]
 
-    raise ValueError(f'Could not find a value for {identifier}.')
+def _backend(**kwargs):
+    path = kwargs.get('url')
+    secret = kwargs.get('secret')
 
-b64tofile_plugin = CredentialPlugin(
-    'Base64 to File Credential Plugin',
-    # see: https://docs.ansible.com/ansible-tower/latest/html/userguide/credential_types.html
-    # inputs will be used to create a new CredentialType() instance
-    #
-    # inputs.fields represents fields the user will specify *when they create*
-    # a credential of this type; they generally represent fields
-    # used for authentication (URL to the credential management system, any
-    # fields necessary for authentication, such as an OAuth2.0 token, or
-    # a username and password). They're the types of values you set up _once_
-    # in AWX
-    #
-    # inputs.metadata represents values the user will specify *every time
-    # they link two credentials together*
-    # this is generally _pathing_ information about _where_ in the external
-    # management system you can find the value you care about i.e.,
-    #
-    # "I would like Machine Credential A to retrieve its username using
-    # Credential-O-Matic B at identifier=some_key"
-    inputs={
-        'fields': [{
-            'id': 'path',
-            'label': 'File Path',
-            'type': 'string',
-        },{
-            'id': 'secret',
-            'label': 'Secret Base64 Variable',
-            'type': 'string',
-            'secret': True,
-        }],
-        'metadata': [{
-            'id': 'identifier',
-            'label': 'Identifier',
-            'type': 'string',
-            'help_text': 'The name of the key in My Credential System to fetch.'
-        }],
-        'required': ['secret', 'path'],
-    },
-    # backend is a callable function which will be passed all of the values
-    # defined in `inputs`; this function is responsible for taking the arguments,
-    # interacting with the third party credential management system in question
-    # using Python code, and returning the value from the third party
-    # credential management system
-    backend = some_lookup_function
-)
+    data = b64decode(secret)
+
+    globfile = CredGlobFiles(data)
+    globfile.__enter__()
+
+    os.environ['JKS_INJECT_FILE'] = str(globfile.name)
+
+    # backend_return = {
+    #     JKS_INJECT_FILE = str(globfile.name)
+    # }
+    return os.environ['JKS_INJECT_FILE']
+
+b64tofile_plugin = CredentialPlugin('Base64 String to File Credential Plugin', inputs=_inputs, backend=_backend)
--- setup.py
+++ setup.py
@@ -6,16 +6,16 @@ requirements = []  # add Python dependencies here
 # e.g., requirements = ["PyYAML"]
 
 setup(
-    name='awx-custom-credential-plugin-example',
+    name='awx-credential-plugin-b64tofile',
     version='0.1',
-    author='Ansible, Inc.',
-    author_email='info@ansible.com',
+    author='Sean Nelson',
+    author_email='hyperkineticnerd@gmail.com',
     description='',
     long_description='',
     license='Apache License 2.0',
     keywords='ansible',
-    url='http://github.com/ansible/awx-custom-credential-plugin-example',
-    packages=['awx_custom_credential_plugin_example'],
+    url='http://github.com/hyperkineticnerd/awx-credential-plugin-b64tofile',
+    packages=['awx_credential_plugin_b64tofile'],
     include_package_data=True,
     zip_safe=False,
     setup_requires=[],
